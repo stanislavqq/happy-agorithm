e1fff266f3eab4add5b9a41e767da8c24e10e76e
bretgauer <bretgauer@obit.ru>
2025-11-11 02:24:12 +0300

    Refactored + Added printing execution time

--- ConsoleApp1/Test.cs
+++ ConsoleApp1/Test.cs
@@ -24,15 +24,19 @@ internal class Test
 
 			string[] input = File.ReadAllLines(fileIn);
 			string[] output = File.ReadAllLines(fileOut);
+			var watch = System.Diagnostics.Stopwatch.StartNew();
 			string x = run(input);
+			watch.Stop();
+			var elapsedMs = watch.ElapsedMilliseconds;
+
 			if (x == output[0])
 			{
-				Console.WriteLine("Тест " + iter + " OK: " + x);
+				Console.WriteLine("Тест " + iter + " OK: " + x + " (Exec. time: ~" + elapsedMs / 1000 + "." + elapsedMs % 1000 + " sec.)");
 			}
 			else
 			{
 				Console.WriteLine("Тест " + iter + " ошибка: " + x +
-								  " ожидалось: " + output[0]);
+								  " ожидалось: " + output[0] + " (Exec. time: ~" + elapsedMs / 1000 + "." + elapsedMs % 1000 + "sec.)");
 			}
 			iter++;
 
